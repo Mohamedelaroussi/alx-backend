f1f684b8e94dc4464919e370bf003f6737184889
‘Mohamedelaroussi’ <midmoha93@gmail.com>
2024-08-27 23:19:40 +0100

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-"""
-Flask app
+"""Module for task 0
 """
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/")
 def index() -> str:
+    """The index function displays the home page of the web application.
+
+    Returns:
+        str: contents of the home page.
     """
-    Handles / route
-    """
-    return render_template('0-index.html')
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(debug=True)
