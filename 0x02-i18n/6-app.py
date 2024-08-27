5fb0b7b079460ce69fab77359c81f7c072e11c48
‘Mohamedelaroussi’ <midmoha93@gmail.com>
2024-08-28 00:13:09 +0100

    6-app.py

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python3
-"""Module for task 6
-"""
-from typing import Dict, Union
-from flask import Flask, render_template, request, g
+"""Basic Flask app"""
+from flask import Flask, g, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
 
-app.url_map.strict_slashes = False
+class Config(object):
+    """Flask configs"""
 
-
-class Config:
-    """Represents a Flask Babel configuration.
-    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 users = {
@@ -28,75 +23,48 @@ users = {
 }
 
 
-@app.route("/")
-def index_6() -> str:
-    """The index function displays the home page of the web application.
-
-    Returns:
-        str: contents of the home page.
-    """
-    return render_template("6-index.html")
-
-
 @babel.localeselector
-def get_locale() -> str:
-    """"Returns the preferred locale for the user.
-
-    The order of priority is:
-    1. Locale from URL parameters
-    2. Locale from user settings
-    3. Locale from request header
-    4. Default locale
-
-    Returns:
-        str: The preferred locale.
-    """
-    # Get the locale parameter from the incoming request
-    locale = request.args.get('locale')
-    # Get list of supported languages from Config
-    supported_languages = app.config["LANGUAGES"]
-    if locale and locale in supported_languages:
-        # If the locale parameter is present and is a supported locale,
-        # return it
+def get_locale():
+    """Get request locale"""
+    locale = request.args.get("locale")
+    langs = app.config["LANGUAGES"]
+    if locale and locale in langs:
         return locale
-    # Locale from user settings
-    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
-        return g.user['locale']
-    header_locale = request.headers.get('locale', '')
-    if header_locale in app.config["LANGUAGES"]:
-        return header_locale
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    if g.user is not None and g.user.get("locale") in langs:
+        return g.user["locale"]
+    if request.headers.get("Accept-Language") is not None:
+        return request.accept_languages.best_match(langs)
+    return app.config["BABEL_DEFAULT_LOCALE"]
 
 
-def get_user() -> Union[Dict, None]:
-    """Returns a user dictionary based on the given ID
-
-    Returns:
-        Union[Dict, None]: The user dictionary if found, otherwise None.
-    """
-    # Get the user_id from the login_as URL parameters
-    login_id = request.args.get('login_as')
-    # If the user ID exists in the URL parameters
-    if login_id:
-        # Get the user dictionary from the `users` dictionary using the user ID
-        return users.get(int(login_id))
-    # If the user ID does not exist in the URL parameters, return None
-    return None
+def get_user(login_as=None):
+    """Return a user dictionary or None"""
+    if login_as is not None:
+        try:
+            return users.get(int(login_as))
+        except ValueError:
+            return None
+    else:
+        return login_as
 
 
 @app.before_request
-def before_request() -> None:
-    """Function to be executed before every request.
+def before_request():
+    """Use `get_user` to find a user if any
+    and set it as a global on `flask.g.user`
     """
-    # Use the get_user function to get the user details
-    user = get_user()
-    # Set the user as a global variable on flask.g
-    g.user = user
+    user = get_user(login_as=request.args.get("login_as"))
+    if user is not None:
+        g.user = user
+    else:
+        g.user = None
 
 
-# Register the before_request function to be executed before every request
-app.before_request(before_request)
+@app.route("/")
+def home():
+    """Home page route"""
+    return render_template("5-index.html", user=g.user)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True))
